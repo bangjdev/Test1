a1ee65f3a00ccee62a988027eb40c71c2204b104
Nguyen Le Ly Bang <bangjdev@gmail.com>
2020-09-12 03:48:34 +0200

    Generated a raw ist of names using pyvi module, including description

--- ner/pyvi_test.py
+++ ner/pyvi_test.py
@@ -6,43 +6,43 @@ from pyvi import ViTokenizer, ViPosTagger
 
 
 def clean_text(text, stopwords):
-	res = []
+    res = []
 
-	for word in text.split(" "):
-		is_stopword = False
-		for stopword in stopwords:
-			if (stopword == word.lower()):
-				is_stopword = True
-				break
-		if not is_stopword:
-			res += [word]
+    for word in text.split(" "):
+        is_stopword = False
+        for stopword in stopwords:
+            if (stopword == word.lower()):
+                is_stopword = True
+                break
+        if not is_stopword:
+            res += [word]
 
 
-	return " ".join(res)
+    return " ".join(res)
 
 
 def extract_name(text, stopwords):
-	tokenized_text = ViTokenizer.tokenize(text)
-	tokenized_text = clean_text(tokenized_text, stopwords)
-	words, tags = ViPosTagger.postagging(tokenized_text)
+    tokenized_text = ViTokenizer.tokenize(text)
+    tokenized_text = clean_text(tokenized_text, stopwords)
+    words, tags = ViPosTagger.postagging(tokenized_text)
 
-	res = []
+    res = []
 
-	for i in range(len(words)):
-		if (tags[i] == "Np"):
-			# print(words[i])
-			res.append(words[i].replace("_", " "))
+    for i in range(len(words)):
+        if (tags[i] == "Np"):
+            # print(words[i])
+            res.append(words[i].replace("_", " "))
 
-	return res
+    return res
 
 
 nltk.download('stopwords')
 
 with open("vietnamese_stop_words.txt", encoding="utf-8") as stopwords_file:
-	stopwords = stopwords_file.readlines()
+    stopwords = stopwords_file.readlines()
 
-	for i in range(len(stopwords)):
-		stopwords[i] = stopwords[i].strip()
+    for i in range(len(stopwords)):
+        stopwords[i] = stopwords[i].strip()
 
 stopwords += sw.words('english')
 
@@ -51,28 +51,34 @@ results = set()
 with open('results.json', 'r', encoding='utf-8') as json_file:
     books = json.load(json_file)
     for book in books:
-    	print("Extracting book title \"" + book['title'] + "\"")
-    	book_reviews = book['reviews']
+        print("Extracting book title \"" + book['title'] + "\"")
 
-    	for review in book_reviews:
-    		# From review's content
-    		print("  => Review of " + review['name'])
-    		review_content = unicodedata.normalize("NFD", review['content'])
-    		extracted_names = extract_name(review_content, stopwords)
-	    	for name in extracted_names:
-	    		results.add(name)
+        print("  => From description")
+        description = unicodedata.normalize("NFD", book['description'])
+        extracted_names = extract_name(description, stopwords)
+        for name in extracted_names:
+            results.add(name)
+
+        book_reviews = book['reviews']
+        # From review's content
+        for review in book_reviews:
+            print("  => Review of " + review['name'])
+            review_content = unicodedata.normalize("NFD", review['content'])
+            extracted_names = extract_name(review_content, stopwords)
+            for name in extracted_names:
+                results.add(name)
 
 
-	    	# From comments
-	    	comments = review['comments']
-	    	for comment in comments:
-	    		print("    > Reply by " + comment['name'])
-	    		comment_content = comment['content']
-	    		extracted_names = extract_name(comment_content, stopwords)
-		    	for name in extracted_names:
-		    		results.add(name)
+            # From comments
+            comments = review['comments']
+            for comment in comments:
+                print("    > Reply by " + comment['name'])
+                comment_content = comment['content']
+                extracted_names = extract_name(comment_content, stopwords)
+                for name in extracted_names:
+                    results.add(name)
 
 
 with open('../names', 'w', encoding='utf-8') as names_file:
-	for word in results:
-		names_file.write(word + "\n")
+    for word in results:
+        names_file.write(word + "\n")
