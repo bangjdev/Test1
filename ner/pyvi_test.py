0107730e7902b76162cbca4516ed052123c2d5a3
Nguyen Le Ly Bang <bangjdev@gmail.com>
2020-09-12 03:28:06 +0200

    Generated a raw ist of names using pyvi module

--- ner/pyvi_test.py
+++ ner/pyvi_test.py
@@ -1,4 +1,7 @@
 import json
+import unicodedata
+import nltk
+from nltk.corpus import stopwords as sw
 from pyvi import ViTokenizer, ViPosTagger
 
 
@@ -6,16 +9,19 @@ def clean_text(text, stopwords):
 	res = []
 
 	for word in text.split(" "):
-		try:
-			pos = stopwords[word.lower()]
-		except:
+		is_stopword = False
+		for stopword in stopwords:
+			if (stopword == word.lower()):
+				is_stopword = True
+				break
+		if not is_stopword:
 			res += [word]
 
 
 	return " ".join(res)
 
 
-def extract_name(text):
+def extract_name(text, stopwords):
 	tokenized_text = ViTokenizer.tokenize(text)
 	tokenized_text = clean_text(tokenized_text, stopwords)
 	words, tags = ViPosTagger.postagging(tokenized_text)
@@ -24,11 +30,13 @@ def extract_name(text):
 
 	for i in range(len(words)):
 		if (tags[i] == "Np"):
-			print(words[i])
-			res.append(words[i])
+			# print(words[i])
+			res.append(words[i].replace("_", " "))
 
 	return res
 
+
+nltk.download('stopwords')
 
 with open("vietnamese_stop_words.txt", encoding="utf-8") as stopwords_file:
 	stopwords = stopwords_file.readlines()
@@ -36,16 +44,34 @@ with open("vietnamese_stop_words.txt", encoding="utf-8") as stopwords_file:
 	for i in range(len(stopwords)):
 		stopwords[i] = stopwords[i].strip()
 
+stopwords += sw.words('english')
 
 results = set()
 
-with open('../results.json', 'r', encoding='utf-8') as json_file:
+with open('results.json', 'r', encoding='utf-8') as json_file:
     books = json.load(json_file)
     for book in books:
-    	description = book['description']
-    	extracted_names = extract_name(clean_text(description, stopwords))
-    	for name in extracted_names:
-    		results.add(name)
+    	print("Extracting book title \"" + book['title'] + "\"")
+    	book_reviews = book['reviews']
+
+    	for review in book_reviews:
+    		# From review's content
+    		print("  => Review of " + review['name'])
+    		review_content = unicodedata.normalize("NFD", review['content'])
+    		extracted_names = extract_name(review_content, stopwords)
+	    	for name in extracted_names:
+	    		results.add(name)
+
+
+	    	# From comments
+	    	comments = review['comments']
+	    	for comment in comments:
+	    		print("    > Reply by " + comment['name'])
+	    		comment_content = comment['content']
+	    		extracted_names = extract_name(comment_content, stopwords)
+		    	for name in extracted_names:
+		    		results.add(name)
+
 
 with open('../names', 'w', encoding='utf-8') as names_file:
 	for word in results:
